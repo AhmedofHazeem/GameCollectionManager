c590e4a4d1a003f81633f5401893aa0fe5610b6e
AhmedofHazeem <ahmedalihazeem24@gmail.com>
2026-01-20 20:02:39 +0300

    CSS Referncing and Style starting

--- controllers/game.routes.js
+++ controllers/game.routes.js
@@ -14,9 +14,25 @@ router.get("/create", async (req, res) => {
   res.render("create-game.ejs", { games });
 });
 
+router.post("/", async (req, res) => {
+  const gameCreated = await Game.create(req.body);
+  //add a display or alert that shows that game added successfully.
+  res.redirect("/games/create");
+});
+
 router.get("/allgames", async (req, res) => {
   const games = await Game.find();
   res.render("all-games.ejs", { games });
 });
+
+//showing one game to the user + its properties
+router.get("/show", async (req, res) => {
+  const oneGame = await Game.find();
+  res.render("show-game.ejs", { oneGame });
+});
+
+//editing a game (by ID) -- GET
+//updating a game (by ID) -- PUT
+//Deleting a game (by ID too) -- DELETE
 
 module.exports = router;
